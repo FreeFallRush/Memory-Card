c7f7fc35022355b70b5d37b92662cb82f79ceb88
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-28 20:28:53 +0300

    Added CardGrid.css in styles folder and styles for CardGrid component
    Added styles in Card.css
    
    Added card-grid-container div in CardGrid.jsx

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,11 +1,14 @@
 import Card from "./Card";
+import "../styles/CardGrid.css";
 
 function CardGrid({ pokemons }) {
   return (
-    <div className="card-grid">
-      {pokemons.map((pokemon, idx) => (
-        <Card key={idx} name={pokemon.name} image={pokemon.image} />
-      ))}
+    <div className="card-grid-container">
+      <div className="card-grid">
+        {pokemons.map((pokemon, idx) => (
+          <Card key={idx} name={pokemon.name} image={pokemon.image} />
+        ))}
+      </div>
     </div>
   );
 }
