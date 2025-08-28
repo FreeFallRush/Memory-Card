53219241cb9f0d17483c55dfd43edf828da00b87
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-28 19:39:23 +0300

    Added components folder in src with Card.jsx file for Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card({ name, image }) {
+  return (
+    <div className="card">
+      <img src={image} alt={name} className="card-img" />
+      <p className="card-name">{name}</p>
+    </div>
+  );
+}
+
+export default Card;
