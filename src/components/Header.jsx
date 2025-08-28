f1ead183c2dc88cf04630e033fdcc801660690ad
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-28 20:46:16 +0300

    Added Header.jsx in components folder with rules button

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header({ onShowRules }) {
+  return (
+    <header>
+      <h1>Pokemon Memory Game</h1>
+      <button className="rules-btn" onClick={onShowRules}>
+        Rules
+      </button>
+    </header>
+  );
+}
+
+export default Header;
