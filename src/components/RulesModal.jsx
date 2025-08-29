08551943e233e429d047418a0ae9bcc3ca498d1b
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-29 21:53:38 +0300

    Added RulesModal.css with modal styles
    Imported styles in RulesModal.jsx and added rules

--- src/components/RulesModal.jsx
+++ src/components/RulesModal.jsx
@@ -1,15 +1,21 @@
-function RulesModal({isOpen, onClose}) {
-    if(!isOpen) return null;
+import "../styles/RulesModal.css";
 
-    return(
-        <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <h2>Game Rules</h2>
-                <p>Click on a Pokemon card.</p>
-                <button className="close-btn" onClick={onClose}>Close</button>
-            </div>
-        </div>
-    )
+function RulesModal({ isOpen, onClose }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <h2>Game Rules</h2>
+        <p>Click on a Pokemon card.</p>
+        <p>To get points, make sure you click each pokemon card only once.</p>
+        <p>Clicking a card twice will reset the score.</p>
+        <button className="close-btn" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default RulesModal;
