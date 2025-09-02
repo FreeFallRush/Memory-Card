8bcb061f1a9093c1a344157c84e1211cd98c7fd2
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-02 14:03:56 +0300

    Modified all jsx files to tsx
    Modified pokemonAPI.js to ts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Scoreboard from "./components/Scoreboard";
 import CardGrid from "./components/CardGrid";
 import Modal from "./components/Modal";
-import Button from "./components/Button";
+import Button from "./components/Buttont";
 import "./App.css";
 
 function App() {
