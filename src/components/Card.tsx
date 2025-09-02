5990748483f5fd9d9a3a2e250ea4852bb21041d4
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-02 14:14:51 +0300

    Added interface for CardProps in Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,12 @@
 import "../styles/Card.css";
 
-function Card({ name, image, onClick }) {
+interface CardProps {
+  name: string;
+  image: string;
+  onClick: () => void;
+}
+
+function Card({ name, image, onClick }: CardProps) {
   return (
     <div className="card" onClick={onClick}>
       <img src={image} alt={name} className="card-img" />
